3257c22ecbd7bf37d8941b96c943d8154eb34fcc
Jeremy Niu <a18201979707@163.com>
2021-12-15 21:26:10 +0800

    fix scanf/scanln exercise

--- scanf/demo1.go
+++ scanf/demo1.go
@@ -1,3 +1,4 @@
+//从控制台获取用户信息。[姓名，年龄，薪资，是否通过考试]
 package main
 
 import (
@@ -18,5 +19,10 @@ func main() {
 	fmt.Println("请确认是否通过:")
 	fmt.Scanln(&isPass)
 
+	fmt.Printf("姓名: %v\n年龄: %v\n薪资: %v\n通过状态: %v\n", name, age, sal, isPass)
+
+	//fmt.Scanf()可以按指定格式输出
+	fmt.Println("请输入您的姓名, 年龄, 薪资, 以及是否通过？") //用空格隔开
+	fmt.Scanf("%s %d %f %t", &name, &age, &sal, &isPass)
 	fmt.Printf("姓名: %v\n年龄: %v\n薪资: %v\n通过状态: %v\n", name, age, sal, isPass)
 }
