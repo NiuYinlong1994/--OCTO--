679840048494c490c637585a1de549944a770c8d
Jeremy Niu <a18201979707@163.com>
2021-12-15 21:10:01 +0800

    update scanln exercise

---
+++ scanf/demo1.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var name string
+	var age int
+	var sal float64
+	var isPass bool
+	fmt.Println("请输入您的姓名:")
+	fmt.Scanln(&name)
+	fmt.Println("请输入您的年龄:")
+	fmt.Scanln(&age)
+	fmt.Println("请输入您的薪资:")
+	fmt.Scanln(&sal)
+	fmt.Println("请确认是否通过:")
+	fmt.Scanln(&isPass)
+
+	fmt.Printf("姓名: %v\n年龄: %v\n薪资: %v\n通过状态: %v\n", name, age, sal, isPass)
+}
